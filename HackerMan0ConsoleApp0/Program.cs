7cc88813ca997dd3878e807566784f4f1225c3e9
Douglas Wallin <wallind@mail.gvsu.edu>
2026-07-22 12:50:54 -0400

    feat: "Write your first C# code" module complete
    https://learn.microsoft.com/en-us/training/modules/csharp-write-first/7-summary#completion

--- HackerMan0ConsoleApp0/Program.cs
+++ HackerMan0ConsoleApp0/Program.cs
@@ -1,3 +1,14 @@
-﻿Console.WriteLine("Hello, Wgsfsdfsdggorld!");
+﻿// Console.WriteLine("Hello, Wgsfsdfsdggorld!");
 
-Console.WriteLine("The current time is: " + DateTime.Now.ToString("hh:mm:ss tt"));
+// Console.WriteLine("The current time is: " + DateTime.Now.ToString("hh:mm:ss tt"));
+
+// Console.WriteLine("This first one uses WriteLine() to print a line of text to the console and a feed return at the end.");
+
+// Console.Write("Yo,");
+// Console.Write(" ");
+// Console.Write("this is a sentence split over multiple Write() calls.");
+
+// Unit 4/7 - Challenge
+
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
