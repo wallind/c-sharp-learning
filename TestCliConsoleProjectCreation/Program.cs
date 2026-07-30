1ba0c9d13ea8c43700bd019677af441f2802bcf0
Douglas Wallin <wallind@mail.gvsu.edu>
2026-07-30 15:38:39 -0400

    feat: "Develop foreach and if else if structures to process array data in C#" module complete
    https://learn.microsoft.com/en-us/training/modules/guided-project-arrays-iteration-selection/7-summary#completion

--- TestCliConsoleProjectCreation/Program.cs
+++ TestCliConsoleProjectCreation/Program.cs
@@ -1,3 +1,4 @@
-﻿Console.WriteLine("XD | Hello, this is HackerMan's World! | 0.0");
-Console.WriteLine("--------------------------------------------");
+﻿using System;
 
+Console.WriteLine("XD | Hello, this is HackerMan's World! | 0.0");
+Console.WriteLine("--------------------------------------------");
