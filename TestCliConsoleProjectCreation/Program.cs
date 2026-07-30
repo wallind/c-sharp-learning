7c1e6d26237586e932373a0e13f2b82c5ea663c6
Douglas Wallin <wallind@mail.gvsu.edu>
2026-07-30 14:42:00 -0400

    feat: "Create effective code comments" module complete
    https://learn.microsoft.com/en-us/training/modules/csharp-readable-code/8-summary#completion

--- TestCliConsoleProjectCreation/Program.cs
+++ TestCliConsoleProjectCreation/Program.cs
@@ -1,1 +1,3 @@
 ﻿Console.WriteLine("XD | Hello, this is HackerMan's World! | 0.0");
+Console.WriteLine("--------------------------------------------");
+
---
+++ completed-microsoft-learn-modules/GetStartedCSharpPart2/CreateEffectiveCodeComments.cs
@@ -0,0 +1,63 @@
+// string firstName = "Bob";
+// int widgetsSold = 7;
+// Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
+
+
+/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+// Random random = new Random();
+// string[] orderIDs = new string[5];
+// // Loop through each blank orderID
+// for (int i = 0; i < orderIDs.Length; i++)
+// {
+// 	// Get a random value that equates to ASCII letters A through E
+// 	int prefixValue = random.Next(65, 70);
+// 	// Convert the random value into a char, then a string
+// 	string prefix = Convert.ToChar(prefixValue).ToString();
+// 	// Create a random number, pad with zeroes
+// 	string suffix = random.Next(1, 1000).ToString("000");
+// 	// Combine the prefix and suffix together, then assign to current OrderID
+// 	orderIDs[i] = prefix + suffix;
+// }
+// // Print out each orderID
+// foreach (var orderID in orderIDs)
+// {
+// 	Console.WriteLine(orderID);
+// }
+
+// Example 1:
+Console
+.
+WriteLine
+(
+"Hello Example 1!"
+)
+;
+
+// Example 2:
+string firstWord = "Hello"; string lastWord = "Example 2"; Console.WriteLine(firstWord + " " + lastWord + "!");
+
+
+Random dice = new Random();
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+	if ((roll1 == roll2) && (roll2 == roll3))
+	{
+		Console.WriteLine("You rolled triples!  +6 bonus to total!");
+		total += 6;
+	}
+	else
+	{
+		Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+		total += 2;
+	}
+}
